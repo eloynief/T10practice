cb3a46ec1c01c3fda03de477ab46906c74cb776f
enieves <enieves@106-02.info2>
2023-05-17 14:31:23 +0200

    terminar djkoifejrojnrfgjiojgtji 4

--- T10/src/ficheros/Ej1.java
+++ T10/src/ficheros/Ej1.java
@@ -1,6 +1,9 @@
 package ficheros;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Ej1 {
 
@@ -8,9 +11,42 @@ public class Ej1 {
 		// TODO Auto-generated method stub
 		
 		//
-		Filereader lector= new FileReader();
+//		
 		
+		try {
+			Scanner sc=new Scanner(new FileReader("C:\\Users\\enieves\\Pictures\\T10practice\\T10\\src\\ficheros\\NumerosReales"));//C:\Users\enieves\Pictures\T10practice\T10\src\ficheros\NumerosReales
+			
+			FileReader lector= new FileReader("C:\\Users\\enieves\\Pictures\\T10practice\\T10\\src\\ficheros\\NumerosReales");
+			
+			int numero = 0;
+			try {
+				
+				while(sc.hasNextInt()) {
+					numero = sc.nextInt();
+					System.out.println(numero);
+				}
 		
+				numero = lector.read();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			int numeros=numero;
+		
+			System.out.println(numero);
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.err.println("mal jesho tontol culo");
+			
+			System.err.println("tenes un error en el bisho ese y la pc se va a oma por kulo");
+			
+			System.out.println("mal jesho");
+			e.getMessage();
+			
+			
+		}
 		
 		
 		
